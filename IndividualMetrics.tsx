071d29769a0f7bb13388a16d5a4402c80defcf10
ykashi0 <y.kashi@live-inc.co.jp>
2025-08-04 18:04:45 +0900

    Update IndividualMetrics.tsx

--- IndividualMetrics.tsx
+++ IndividualMetrics.tsx
@@ -1,7 +1,7 @@
 
 import React, { useState, useEffect } from 'react';
-import Card from './Card';
-import type { SalesMetric } from '../types';
+import Card from './Card.tsx';
+import type { SalesMetric } from '../types.ts';
 
 interface IndividualMetricsProps {
   title: string;
